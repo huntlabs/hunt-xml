78a7db6678e6b3895f2c4dde996b61a7134c9158
heromyth <bitworld@qq.com>
2019-11-29 15:04:58 +0800

    Clear up the Simple example.

--- examples/Simple/source/app.d
+++ examples/Simple/source/app.d
@@ -2,37 +2,11 @@ import hunt.xml;
 
 import std.file;
 import std.path;
-import std.stdio;
-
-import hunt.logging.ConsoleLogger;
-
-import std.ascii;
-
-import hunt.xml.Internal;
 
 void main() {
-
 	string rootPath = dirName(thisExePath());
 	string fullName = buildPath(rootPath, "resources/books.xml");
-	// string fullName = buildPath(rootPath, "books.xml");
-	trace(fullName);
 	string data = readText(fullName);
-	// trace(data);
 	Document document = Document.parse(data);
-
-	auto file = File("output.xml", "w");
-	auto ltw = file.lockingTextWriter;
-	Writer!(File.LockingTextWriter, PrettyPrinters.Indenter) writer = buildWriter(ltw, PrettyPrinters.Indenter());
-	// auto writer = buildWriter(ltw, PrettyPrinters.Minimalizer());
-
-	writer.write(document);
-
-	// string str = document.toString();
-	// trace(str);
-
-	// str = document.toPrettyString();
-	// trace(str);
-
-	// document.toFile("output.xml");
-
+	document.toFile("output.xml");
 }
