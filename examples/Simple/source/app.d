9b0020008f5e16cea36a5da6cd8879f5dbd26397
heromyth <bitworld@qq.com>
2022-05-19 21:17:46 +0800

    Test for node iteration.

--- examples/Simple/source/app.d
+++ examples/Simple/source/app.d
@@ -1,13 +1,18 @@
 import hunt.xml;
 import std.stdio;
 
+import hunt.logging;
+
+import std.array : Appender;
+
 void main() {
 	// readAndWrite();
 	// loadAndSave();
 	// objectToXml();
 	// xmlToObject();
 	// testStruct();
-	escapeTest();
+	// escapeTest();
+	iterationNodes();
 }
 
 void escapeTest() {
@@ -18,10 +23,73 @@ void escapeTest() {
         </receiverInfo>	
 `;
 
+
 	Document doc = Document.parse(text);
 
 	writeln(doc.toPrettyString());
 
+}
+
+void iterationNodes() {
+
+	string text = `
+<xml>
+<appid><![CDATA[wx50b8ff9117eb0d67]]></appid>
+<transaction_id><![CDATA[4200001427202205084516212946]]></transaction_id>
+<trade_type><![CDATA[JSAPI]]></trade_type>
+<total_fee>800</total_fee>
+<time_end><![CDATA[20220508155047]]></time_end>
+<sign><![CDATA[3B87F4295E36E7E5EBE128D9B64638D5]]></sign>
+<return_code><![CDATA[SUCCESS]]></return_code>
+<result_code><![CDATA[SUCCESS]]></result_code>
+<out_trade_no><![CDATA[011651996147427170]]></out_trade_no>
+<openid><![CDATA[opEGh5a8ZBYO0GTx9lwodZ2edfYo]]></openid>
+<nonce_str><![CDATA[FPI6Da2bJOqQyeIN]]></nonce_str>
+<mch_id><![CDATA[1607490068]]></mch_id>
+<is_subscribe><![CDATA[N]]></is_subscribe>
+<fee_type><![CDATA[CNY]]></fee_type>
+<coupon_id_0><![CDATA[33390755975]]></coupon_id_0>
+<coupon_fee_0><![CDATA[600]]></coupon_fee_0>
+<coupon_fee>600</coupon_fee>
+<coupon_count><![CDATA[1]]></coupon_count>
+<cash_fee><![CDATA[200]]></cash_fee>
+<bank_type><![CDATA[COMM_DEBIT]]></bank_type>
+</xml>
+`;
+
+	Document doc = Document.parse(text);
+
+	Element el = doc.firstNode;
+
+	string key;
+	string value;
+	string[string] nodes;
+
+	el = el.firstNode();
+	while(el !is null) {
+
+		Element subNode = el.firstNode();
+		// tracef("type: %s", subNode.getType());
+
+		key = el.getName();
+
+		if(subNode.getType() == NodeType.Text) {
+			value = el.getText();
+		} else if(subNode.getType() == NodeType.CDATA) {
+			Appender!string appender = Appender!string();
+			auto writer = buildWriter(appender, PrettyPrinters.Minimalizer());
+			writer.writeNode(subNode);
+			value = appender.data;
+		} else {
+			throw new Exception("Can't handle this xml");
+		}
+		writefln("key: %s, value: %s", key, value);
+		nodes[key] = value;
+
+		el = el.nextSibling();
+	}
+
+	writeln(nodes);
 }
 
 // read/write
